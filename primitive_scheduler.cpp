814c09c662a70c35e104d21ba7b1e8c945616bec
astroeng <dschacht@gmail.com>
2015-04-23 22:42:34 -0400

    Updated comments to better explain the scheduler.
    Added the ability for the scheduler to "kill" an offending "task"
    Added some functions to grab stats on the schedule and tasks.
    - How long a task takes to execute
    - How often the scheduler is being called
    - If a task has been killed
    - How many tasks are in the schedule

--- primitive_scheduler.cpp
+++ primitive_scheduler.cpp
@@ -16,40 +16,127 @@ PrimitiveScheduler::PrimitiveScheduler()
 {
   emptyPosition = 0;
   current_time = millis();
+  interval_time = 0;
 }
 
+/* This function will add a task to the schedule. A function name should be
+ * passed as the first parameter, the second parameter is the interval in 
+ * milliseconds that the function should be run.
+ */
 char PrimitiveScheduler::addTask(func_ptr new_func, unsigned long interval)
 {
+  
   if (emptyPosition < MAX_TASK)
   {
-    tasks[emptyPosition] = new_func;
-    taskInterval[emptyPosition] = interval;
+    tasks[emptyPosition]             = new_func;
+    taskInterval[emptyPosition]      = interval;
+    taskExecutionTime[emptyPosition] = 0;
+    taskSkipped[emptyPosition]       = 0;
   
     emptyPosition++;
     
     return ADD_SUCCESS;
+
   }
   return ADD_FAIL;
+
 }
 
-
+/* This will return the time from the scheduler's point of view.
+ */
 unsigned long PrimitiveScheduler::getTime()
 {
   return current_time;
 }
 
+/* This will return how often the scheduler is called. 
+ */
+unsigned long PrimitiveScheduler::getIntervalTime()
+{
+  return interval_time;
+}
 
+/* This will return the execution time of a particular task.
+ */
+unsigned long PrimitiveScheduler::getTaskExecutionTime(char i)
+{
+  return taskExecutionTime[i];
+}
+
+/* This will return the status of a task. If a task is skipped this
+ * will be greater than 0, and set to the time the task ran over in
+ * milliseconds. Once skipped a task will not be run again until reset.
+ */
+unsigned long PrimitiveScheduler::getTaskSkipped(char i)
+{
+  return taskSkipped[i];
+}
+
+/* This will return the current number of tasks. 
+ */
+unsigned char PrimitiveScheduler::getTaskCount()
+{
+  return emptyPosition;
+}
+
+/* This will reset a task, effectivly adding it back into the 
+ * schedule.
+ */
+void resetTask(char i, int additional_time)
+{
+  taskInterval[i] += additional_time;
+  taskSkipped[i] = 0;
+}
+
+/* This is the main part of the scheduler and should be called from the
+ * Arduino loop function.
+ */
 void PrimitiveScheduler::run()
 {
-  int i;
-  current_time = millis();
+  
+  unsigned long start_time;
+  char i;
+  
+  /* Compute how long it has been since the last time the scheduler was run.
+   */
+  
+  start_time = millis();
+  interval_time = start_time - current_time;
+  current_time = start_time;
+  
+  /* Run throught the array of "tasks" and run them. */
   
   for (i = 0; i < emptyPosition; i++)
   {
-    if ((taskLastExecution[i] + taskInterval[i]) <= current_time)
+    
+    /* Check to see if enough time has elapsed since the last time the
+     * "task" was run. Also, make sure that the "task" is behaving. Basically
+     * if the "task" takes 100ms to run and you tell it to run every 20ms
+     * this will catch that and not run the "task".
+     */
+    
+    if ((taskLastExecution[i] + taskInterval[i]) <= current_time &&
+        (taskExecutionTime[i] < taskInterval[i]))
     {
+      
+      start_time = millis();
       ((void (*)()) tasks[i])();
       taskLastExecution[i] = current_time;
+      taskExecutionTime[i] = millis() - start_time;
+      
     }
+    
+    /* If a "task" has been found to be misbehaving skip it and set the skipped
+     * value to be the amount of time the task ran over.
+     */
+    
+    else if (taskExecutionTime[i] >= taskInterval[i])
+    {
+      
+      taskSkipped[i] = taskExecutionTime[i] - taskInterval[i];
+    
+    }
+    
   }
+  
 }
--- primitive_scheduler.h
+++ primitive_scheduler.h
@@ -8,6 +8,23 @@
  *************************************************************
  */
 
+
+/* This is a primitive scheduler for the Arduino platform. It is very
+ * basic only allowing a few tasks to be run on a given periodicity. The
+ * period is not guarateed since the shortest interval is dictated by 
+ * the execution time of all of the schedule tasks. If the first task
+ * is supposed to run every 10ms but the second task takes 30ms to 
+ * perform it's job then the first task will be delayed by 30ms. However,
+ * if a task is setup to run every 15ms and really takes 20ms to execute.
+ * Then this scheduler will ignore the task until it is reset.
+ * 
+ * This scheduler is not preemptive!
+ * This scheduler has no concept of priority!
+ * This scheduler will "kill" a misbehaving task!
+ * This scheduler is simple and crude!!
+ * This scheduler makes it very simple to read IO on a schedule.
+ */
+
 #ifndef PRIMITIVE_SCHEDULER_H
 #define PRIMITIVE_SCHEDULER_H
 
@@ -25,16 +42,27 @@ public:
 
   char addTask(func_ptr new_func, unsigned long interval);
   unsigned long getTime();
+  unsigned long getIntervalTime();
+  unsigned long getTaskExecutionTime(char i);
+  unsigned long getTaskSkipped(char i);
+  unsigned char getTaskCount();
+
+  void resetTask(char i, int additional_time);
   void run();
   
 private:
 
   unsigned long current_time;
+  unsigned long interval_time;
   unsigned long taskInterval[MAX_TASK];
+  unsigned long taskExecutionTime[MAX_TASK];
   unsigned long taskLastExecution[MAX_TASK];
+  unsigned long taskSkipped[MAX_TASK];
+  
   func_ptr tasks[MAX_TASK];
   
   unsigned char emptyPosition;
+
 };
 
 #endif
